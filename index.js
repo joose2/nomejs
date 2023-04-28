12c95f1d0cff6e29578a7493ead9e4c63eae1451
jose helder <jose.silva2000@etec.sp.gov.br>
2023-04-27 22:36:06 -0300

    atividade sobre javescript

---
+++ index.js
@@ -0,0 +1,9 @@
+let nome = prompt ("Digite seu primeiro nome");
+let sobrenome = prompt ("Digite seu sobrenome");
+
+let nomeCompleto = `${nome} ${sobrenome}`;
+let nomeCatalogo = `${sobrenome.toUpperCase()} ${nome}`;
+
+
+alert (nomeCompleto);
+alert (nomeCatalogo);
